e5f18c1e256ea24b612d6f383bfd3909ca579190
2306-davisk <2306_KAVYAM@davkharghar.net>
2022-02-21 17:49:06 +0530

    21/02/2022 - Preview  Social Portfoilio

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,8 @@
 /* Button */
 function on_Social_Portfolio() {
-    document.getElementById("file").style.display = "block";
+    document.getElementById("preview_Social_Portfolio").style.display = "block";
 }
   
-function off_test() {
-    document.getElementById("file").style.display = "none";
+function off_Social_Portfolio() {
+    document.getElementById("preview_Social_Portfolio").style.display = "none";
 }
