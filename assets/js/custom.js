ea3259645dab65a4193eaf8ba2cf0e54d5697810
2306-davisk <2306_KAVYAM@davkharghar.net>
2022-02-21 17:33:55 +0530

    21/02/2022 - Preview Test

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+/* Button */
+function on_Social_Portfolio() {
+    document.getElementById("file").style.display = "block";
+}
+  
+function off_test() {
+    document.getElementById("file").style.display = "none";
+}
