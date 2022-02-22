02bfb0eb020e8d91140e78d383822df5cc74042b
2306-davisk <2306_KAVYAM@davkharghar.net>
2022-02-22 12:08:00 +0530

    22/02/2022 - Changes and Addition
    Addition -
    Added Preview to -
    English.html
    Science.html
    Social.html
    
    File Added -
    Science -
    May Vacation Assignment (PPTX & PDF)
    Portfolio (PPTX & PDF)
    Social -
    Portfolio (PDF)
    
    Changes -
    Custom.css - Preview Sync
    Custom.js - Preview Sync

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,130 @@
 /* Button */
-function on_Social_Portfolio() {
-    document.getElementById("preview_Social_Portfolio").style.display = "block";
-}
-  
-function off_Social_Portfolio() {
-    document.getElementById("preview_Social_Portfolio").style.display = "none";
-}
+                        /*
+                        <section id="+++">
+                            <div>
+                                <span class="text-end" style="margin-left: 95% !important;"><button class="closebtn" onclick="of+++()">×</button></span>
+                                <br>
+                            </div>
+                            <div class="card mb-3" style="margin: auto !important; align-self: center !important; border: none !important;">
+                                <iframe src="+file-link+" width="100%" height="500px" class="semi-btn" style="margin: auto !important; align-self: center !important;"></iframe>
+                            </div>
+                        </section>
+                        */
+
+/* Social */
+        function on_Social_Portfolio() {
+            document.getElementById("preview_Social_Portfolio").style.display = "block";
+        }
+        function off_Social_Portfolio() {
+            document.getElementById("preview_Social_Portfolio").style.display = "none";
+        }
+
+    /* End Social */
+
+    
+/* English */
+
+        /* Activities */
+
+            /* First Flight */
+
+                /* Ch 1.1 - A Letter to God */
+                    function on_English_Activity_FF_Letter_to_God() {
+                        document.getElementById("preview_English_Activity_FF_Letter_to_God").style.display = "block";
+                    }
+                    function off_English_Activity_FF_Letter_to_God() {
+                        document.getElementById("preview_English_Activity_FF_Letter_to_God").style.display = "none";
+                    }
+
+                /* Ch 1.2 - Dust of Snow */
+                    function on_English_Activity_FF_Dust_of_Snow() {
+                        document.getElementById("preview_English_Activity_FF_Dust_of_Snow").style.display = "block";
+                    }
+                    function off_English_Activity_FF_Dust_of_Snow() {
+                        document.getElementById("preview_English_Activity_FF_Dust_of_Snow").style.display = "none";
+                    }
+
+                /* Ch 1.3 - Fire and Ice */
+                    function on_English_Activity_FF_Fire_and_Ice() {
+                        document.getElementById("preview_English_Activity_FF_Fire_and_Ice").style.display = "block";
+                    }
+                    function off_English_Activity_FF_Fire_and_Ice() {
+                        document.getElementById("preview_English_Activity_FF_Fire_and_Ice").style.display = "none";
+                    }
+
+                /* Ch 2.1 - Nelson Mandela */
+                    function on_English_Activity_FF_Nelson_Mandela() {
+                        document.getElementById("preview_English_Activity_FF_Nelson_Mandela").style.display = "block";
+                    }
+                    function off_English_Activity_FF_Nelson_Mandela() {
+                        document.getElementById("preview_English_Activity_FF_Nelson_Mandela").style.display = "none";
+                    }
+                    
+                    
+            /* Footprints Without Feet */
+                /* Ch 1 - Triumph of Surgery */
+                    function on_English_Activity_FF_Triumph_of_Surgery() {
+                        document.getElementById("preview_English_Activity_FF_Triumph_of_Surgery").style.display = "block";
+                    }
+                    function off_English_Activity_FF_Triumph_of_Surgery() {
+                        document.getElementById("preview_English_Activity_FF_Triumph_of_Surgery").style.display = "none";
+                    }
+
+            /* Vaction Assignment */
+            function on_English_Activity_Vacation() {
+                document.getElementById("preview_English_Activity_Vacation_Assignment").style.display = "block";
+            }
+            function off_English_Activity_Vacation() {
+                document.getElementById("preview_English_Activity_Vacation_Assignment").style.display = "none";
+            }
+
+        /* Portfolio */
+        function on_English_Portfolio() {
+            document.getElementById("preview_English_Portfolio").style.display = "block";
+        }
+        function off_English_Portfolio() {
+            document.getElementById("preview_English_Portfolio").style.display = "none";
+        }
+    
+/* Science */
+    
+        /* Practicals */
+            /* 1 */
+            function on_S_P_one() {
+                document.getElementById("preview_S_P_one").style.display = "block";
+            }
+            function off_S_P_one() {
+                document.getElementById("preview_S_P_one").style.display = "none";
+            }
+
+            /* 2 */
+            function on_S_P_two() {
+                document.getElementById("preview_S_P_two").style.display = "block";
+            }
+            function off_S_P_two() {
+                document.getElementById("preview_S_P_two").style.display = "none";
+            }
+
+            /* 5 */
+            function on_S_P_five() {
+                document.getElementById("preview_S_P_five").style.display = "block";
+            }
+            function off_S_P_five() {
+                document.getElementById("preview_S_P_five").style.display = "none";
+            }
+            
+            /* 6 */
+            function on_S_P_six() {
+                document.getElementById("preview_S_P_six").style.display = "block";
+            }
+            function off_S_P_six() {
+                document.getElementById("preview_S_P_six").style.display = "none";
+            }
+
+            /* Ohm's Law */
+            function on_S_P_OL() {
+                document.getElementById("preview_S_P_OL").style.display = "block";
+            }
+            function off_S_P_OL() {
+                document.getElementById("preview_S_P_OL").style.display = "none";
+            }
