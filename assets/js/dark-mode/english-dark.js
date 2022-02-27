e58745cfddceba82247749955a42b798d33df223
2306-davisk <2306_KAVYAM@davkharghar.net>
2022-02-27 16:43:48 +0530

    27/02/2022 - Minify JS

--- assets/js/dark-mode/english-dark.js
+++
@@ -1,7 +0,0 @@
-function on_dark_english()  {
-    /* Off Button */
-    document.getElementById("off_button").style.display = "block";
-    document.getElementById("on_button").style.display = "none";
-    /* Navbar */
-    document.getElementById("nav_head").classList.remove("bg-white");   document.getElementById("nav_head").classList.add("bg-dark")
-}
