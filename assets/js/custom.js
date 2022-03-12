7ef819e085ef466dbf56f7539b67ea6b3998b27c
2306-davisk <2306_KAVYAM@davkharghar.net>
2022-03-12 15:11:52 +0530

    12/03/2022 - Addition Preview Transition
    Transition added to -
    English.html
    Maths.html
    Science.html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,7 +12,7 @@
                         */
 
 /* Social */
-        function on_Social_Portfolio() {document.getElementById("preview_Social_Portfolio").style.height = '100%';}
+        function on_Social_Portfolio() {document.getElementById("preview_Social_Portfolio").style.height = '600px';}
         function off_Social_Portfolio() {document.getElementById("preview_Social_Portfolio").style.height = "0px";}
 
     /* End Social */
@@ -25,90 +25,90 @@
             /* First Flight */
 
                 /* Ch 1.1 - A Letter to God */
-                    function on_English_Activity_FF_Letter_to_God() {document.getElementById("preview_English_Activity_FF_Letter_to_God").style.display = "block";}
-                    function off_English_Activity_FF_Letter_to_God() {document.getElementById("preview_English_Activity_FF_Letter_to_God").style.display = "none";}
+                    function on_English_Activity_FF_Letter_to_God() {document.getElementById("preview_English_Activity_FF_Letter_to_God").style.height = '600px';}
+                    function off_English_Activity_FF_Letter_to_God() {document.getElementById("preview_English_Activity_FF_Letter_to_God").style.height = "0px";}
 
                 /* Ch 1.2 - Dust of Snow */
-                    function on_English_Activity_FF_Dust_of_Snow() {document.getElementById("preview_English_Activity_FF_Dust_of_Snow").style.display = "block";}
-                    function off_English_Activity_FF_Dust_of_Snow() {document.getElementById("preview_English_Activity_FF_Dust_of_Snow").style.display = "none";}
+                    function on_English_Activity_FF_Dust_of_Snow() {document.getElementById("preview_English_Activity_FF_Dust_of_Snow").style.height = '600px';}
+                    function off_English_Activity_FF_Dust_of_Snow() {document.getElementById("preview_English_Activity_FF_Dust_of_Snow").style.height = "0px";}
 
                 /* Ch 1.3 - Fire and Ice */
-                    function on_English_Activity_FF_Fire_and_Ice() {document.getElementById("preview_English_Activity_FF_Fire_and_Ice").style.display = "block";}
-                    function off_English_Activity_FF_Fire_and_Ice() {document.getElementById("preview_English_Activity_FF_Fire_and_Ice").style.display = "none";}
+                    function on_English_Activity_FF_Fire_and_Ice() {document.getElementById("preview_English_Activity_FF_Fire_and_Ice").style.height = '600px';}
+                    function off_English_Activity_FF_Fire_and_Ice() {document.getElementById("preview_English_Activity_FF_Fire_and_Ice").style.height = "0px";}
 
                 /* Ch 2.1 - Nelson Mandela */
-                    function on_English_Activity_FF_Nelson_Mandela() {document.getElementById("preview_English_Activity_FF_Nelson_Mandela").style.display = "block";}
-                    function off_English_Activity_FF_Nelson_Mandela() {document.getElementById("preview_English_Activity_FF_Nelson_Mandela").style.display = "none";}
+                    function on_English_Activity_FF_Nelson_Mandela() {document.getElementById("preview_English_Activity_FF_Nelson_Mandela").style.height = '600px';}
+                    function off_English_Activity_FF_Nelson_Mandela() {document.getElementById("preview_English_Activity_FF_Nelson_Mandela").style.height = "0px";}
                     
                     
             /* Footprints Without Feet */
                 /* Ch 1 - Triumph of Surgery */
-                    function on_English_Activity_FF_Triumph_of_Surgery() {document.getElementById("preview_English_Activity_FF_Triumph_of_Surgery").style.display = "block";}
-                    function off_English_Activity_FF_Triumph_of_Surgery() {document.getElementById("preview_English_Activity_FF_Triumph_of_Surgery").style.display = "none";}
+                    function on_English_Activity_FF_Triumph_of_Surgery() {document.getElementById("preview_English_Activity_FF_Triumph_of_Surgery").style.height = '600px';}
+                    function off_English_Activity_FF_Triumph_of_Surgery() {document.getElementById("preview_English_Activity_FF_Triumph_of_Surgery").style.height = "0px";}
 
             /* Vaction Assignment */
-            function on_English_Activity_Vacation() {document.getElementById("preview_English_Activity_Vacation_Assignment").style.display = "block";}
-            function off_English_Activity_Vacation() {document.getElementById("preview_English_Activity_Vacation_Assignment").style.display = "none";}
+            function on_English_Activity_Vacation() {document.getElementById("preview_English_Activity_Vacation_Assignment").style.height = '600px';}
+            function off_English_Activity_Vacation() {document.getElementById("preview_English_Activity_Vacation_Assignment").style.height = "0px";}
 
         /* Portfolio */
-        function on_English_Portfolio() {document.getElementById("preview_English_Portfolio").style.display = "block";}
-        function off_English_Portfolio() {document.getElementById("preview_English_Portfolio").style.display = "none";}
+        function on_English_Portfolio() {document.getElementById("preview_English_Portfolio").style.height = "600px";}
+        function off_English_Portfolio() {document.getElementById("preview_English_Portfolio").style.height = "0px";}
     
 /* Science */
     
         /* Practicals */
             /* 1 */
-            function on_S_P_one() {document.getElementById("preview_S_P_one").style.display = "block";}
-            function off_S_P_one() {document.getElementById("preview_S_P_one").style.display = "none";}
+            function on_S_P_one() {document.getElementById("preview_S_P_one").style.height = "600px";}
+            function off_S_P_one() {document.getElementById("preview_S_P_one").style.height = "0px";}
 
             /* 2 */
-            function on_S_P_two() {document.getElementById("preview_S_P_two").style.display = "block";}
-            function off_S_P_two() {document.getElementById("preview_S_P_two").style.display = "none";}
+            function on_S_P_two() {document.getElementById("preview_S_P_two").style.height = "600px";}
+            function off_S_P_two() {document.getElementById("preview_S_P_two").style.height = "0px";}
             
             /* 5 */
-            function on_S_P_five() {document.getElementById("preview_S_P_five").style.display = "block";}
-            function off_S_P_five() {document.getElementById("preview_S_P_five").style.display = "none";}
+            function on_S_P_five() {document.getElementById("preview_S_P_five").style.height = "600px";}
+            function off_S_P_five() {document.getElementById("preview_S_P_five").style.height = "0px";}
             
             /* 6 */
-            function on_S_P_six() {document.getElementById("preview_S_P_six").style.display = "block";}
-            function off_S_P_six() {document.getElementById("preview_S_P_six").style.display = "none";}
+            function on_S_P_six() {document.getElementById("preview_S_P_six").style.height = "600px";}
+            function off_S_P_six() {document.getElementById("preview_S_P_six").style.height = "0px";}
 
             /* Ohm's Law */
-            function on_S_P_OL() {document.getElementById("preview_S_P_OL").style.display = "block";}
-            function off_S_P_OL() {document.getElementById("preview_S_P_OL").style.display = "none";}
+            function on_S_P_OL() {document.getElementById("preview_S_P_OL").style.height = "600px";}
+            function off_S_P_OL() {document.getElementById("preview_S_P_OL").style.height = "0px";}
 
 
 /* Maths */
         /* Term 1 */
             /* Art Integretion Activity */
-            function on_M_AIA() {document.getElementById("preview_M_AIA").style.display = "block";}
-            function off_M_AIA() {document.getElementById("preview_M_AIA").style.display = "none";}
+            function on_M_AIA() {document.getElementById("preview_M_AIA").style.height = "600px";}
+            function off_M_AIA() {document.getElementById("preview_M_AIA").style.height = "0px";}
             /* Real Numbers */
-            function on_M_RN() {document.getElementById("preview_M_RN").style.display = "block";}
-            function off_M_RN() {document.getElementById("preview_M_RN").style.display = "none";}
+            function on_M_RN() {document.getElementById("preview_M_RN").style.height = "600px";}
+            function off_M_RN() {document.getElementById("preview_M_RN").style.height = "0px";}
 
         /* Term 2 */
             /* Clintometer - Trignometry */
-            function on_M_CLN() {document.getElementById("preview_M_CLN").style.display = "block";}
-            function off_M_CLN() {document.getElementById("preview_M_CLN").style.display = "none";}
+            function on_M_CLN() {document.getElementById("preview_M_CLN").style.height = "600px";}
+            function off_M_CLN() {document.getElementById("preview_M_CLN").style.height = "0px";}
 
             /* Arithmetic Progression - Graph */
-            function on_M_AP() {document.getElementById("preview_M_AP").style.display = "block";}
-            function off_M_AP() {document.getElementById("preview_M_AP").style.display = "none";}
+            function on_M_AP() {document.getElementById("preview_M_AP").style.height = "600px";}
+            function off_M_AP() {document.getElementById("preview_M_AP").style.height = "0px";}
 
             /* Circle - Tangents of the Circle */
-            function on_M_CRCL() {document.getElementById("preview_M_CRCL").style.display = "block";}
-            function off_M_CRCL() {document.getElementById("preview_M_CRCL").style.display = "none";}
+            function on_M_CRCL() {document.getElementById("preview_M_CRCL").style.height = "600px";}
+            function off_M_CRCL() {document.getElementById("preview_M_CRCL").style.height = "0px";}
 
             /* 3D Model - Surface Area and Volume */
-            function on_M_3D() {document.getElementById("preview_M_3D").style.display = "block";}
-            function off_M_3D() {document.getElementById("preview_M_3D").style.display = "none";}
+            function on_M_3D() {document.getElementById("preview_M_3D").style.height = "600px";}
+            function off_M_3D() {document.getElementById("preview_M_3D").style.height = "0px";}
 
         /* Single PDF */
             /* Term 1 */
-            function on_M_T1()  {document.getElementById("preview_M_T1").style.display = "block";}
-            function off_M_T1() {document.getElementById("preview_M_T1").style.display = "none";}
+            function on_M_T1()  {document.getElementById("preview_M_T1").style.height = "600px";}
+            function off_M_T1() {document.getElementById("preview_M_T1").style.height = "0px";}
 
             /* Term 2 */
-            function on_M_T2()  {document.getElementById("preview_M_T2").style.display = "block";}
-            function off_M_T2() {document.getElementById("preview_M_T2").style.display = "none";}
+            function on_M_T2()  {document.getElementById("preview_M_T2").style.height = "600px";}
+            function off_M_T2() {document.getElementById("preview_M_T2").style.height = "0px";}
